f890f1decbff4b71c78a9eb295e6d64152709f93
PJMantoss <joelptoss@gmail.com>
2022-09-09 20:38:56 +0100

    update Questions

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -3,7 +3,7 @@ import React, {useState, useEffect} from 'react';
 const Questions = () => {
   const [questions, setQuestions] = useState([]);
 
-  useEffect(() => {}, [])
+  useEffect(() => {}, []);
 
   return (
     <div>
