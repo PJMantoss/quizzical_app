189c4dfa53750aac2d662a6386dfec6045aad518
PJMantoss <joelptoss@gmail.com>
2022-09-10 18:42:50 +0100

    update Intro

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Intro = () => {
   return (
-    <div className=''>
+    <div className='intro'>
         <h1 className=''>Quizzical</h1>
         <p className=''>Have fun picking your brain</p>
         <button className=''>Start Quiz</button>
