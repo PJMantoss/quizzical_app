358cae62d830cfa5215f895c82ec7b76f7bf3f9d
PJMantoss <joelptoss@gmail.com>
2022-09-10 20:25:06 +0100

    update Question styling

--- src/components/Question.js
+++ src/components/Question.js
@@ -5,10 +5,10 @@ const Question = () => {
     <div className='questionContainer'>
       <h1 className='question'>Question</h1>
       <div className='answersContainer'>
-        <span className='answersContainer'>Ans1</span>
-        <span className='answersContainer'>Ans2</span>
-        <span className='answersContainer'>Ans3</span>
-        <span className='answersContainer'>Ans4</span>
+        <span className='answersBtn'>Ans1</span>
+        <span className='answersBtn'>Ans2</span>
+        <span className='answersBtn'>Ans3</span>
+        <span className='answersBtn'>Ans4</span>
       </div>
     </div>
   )
