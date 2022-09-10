f5e3551ff7692821e2f2f34b7898e18805c3eefb
PJMantoss <joelptoss@gmail.com>
2022-09-10 19:59:46 +0100

    update Question

--- src/components/Question.js
+++ src/components/Question.js
@@ -3,8 +3,11 @@ import React from 'react';
 const Question = () => {
   return (
     <div className='questionContainer'>
-      <h1 className='question'></h1>
-      <div className='answersContainer'></div>
+      <h1 className='question'>Q</h1>
+      <div className='answersContainer'>
+        <span>Ans1</span>
+        <span>Ans1</span>
+      </div>
     </div>
   )
 }
