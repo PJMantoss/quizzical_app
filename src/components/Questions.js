392cbb3f3e55a8d02d406f4c8fab66fa0864e746
PJMantoss <joelptoss@gmail.com>
2022-09-10 16:10:36 +0100

    update Questions

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 
 const API = "https://opentdb.com/api.php?amount=5&category=30&difficulty=medium&type=multiple";
+console.log(API);
 
 const Questions = () => {
   const [questions, setQuestions] = useState([]);
