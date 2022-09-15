df89b44e4b06e7b1b9dbbeea46e1dca75e5992e2
PJMantoss <joelptoss@gmail.com>
2022-09-15 17:45:23 +0100

    update Intro

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,11 +1,16 @@
 import React from 'react'
 
-const Intro = () => {
+const Intro = (props) => {
   return (
     <div className='intro'>
         <h1 className='heading'>Quizzical</h1>
         <p className='desc'>Have fun picking your brain</p>
-        <button className='startBtn'>Start Quiz</button>
+        <button 
+          className='startBtn'
+          onClick={() => props.setShowQuestions(true)}
+        >
+          Start Quiz
+        </button>
     </div>
   )
 }
